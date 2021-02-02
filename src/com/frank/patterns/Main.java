f7b79efadd1b4625ef96f62c40948b345e3ff290
Frank Wang <fran0532@gmail.com>
2021-02-02 06:59:21 -0500

    commit1

---
+++ src/com/frank/patterns/Main.java
@@ -0,0 +1,16 @@
+package com.frank.patterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String msg = "CE2";
+        System.out.printf("Hello Intellige%s%n", msg);
+
+
+
+    }
+    protected int getAge(String[] str){
+        return 55;
+    }
+}
---
+++ src/com/frank/patterns/temp.java
@@ -0,0 +1,15 @@
+package com.frank.patterns;
+
+public class temp {
+    public temp() {
+    }
+    public int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}
